2fb378a9848ea104639760c098cabbce5d5b6a19
sonastea <sonastea@gmail.com>
2022-08-12 20:13:36 -0700

    Update withTRPC url env variable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,14 +23,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 function getBaseUrl() {
   if (typeof window === "undefined") return ""; // Browser should use current path
-  if (process.env.NEXT_PUBLIC_VERCEL_URL)
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://${window.location.hostname}:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 }
 
 export default withTRPC<AppRouter>({
-  config({ ctx }) {
+  config() {
     const url = `${getBaseUrl()}/api/trpc`;
 
     return {
