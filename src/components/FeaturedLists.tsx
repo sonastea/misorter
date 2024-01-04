27a37fb7a2c2905acadba282333db20bf0bebb2f
sonastea <sonastea@gmail.com>
2024-01-04 00:58:21 -0800

    feat: featured lists class names from module css
    - Refactor dark & dark-faded versions of text-once as a var in global.css

--- src/components/FeaturedLists.tsx
+++ src/components/FeaturedLists.tsx
@@ -1,5 +1,6 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
+import styles from "../styles/FeaturedLists.module.css";
 
 const FeaturedLists = ({
   open,
@@ -29,10 +30,7 @@ const FeaturedLists = ({
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <div
-          className="fixed inset-0 bg-light-bg-secondary dark:bg-dark-bg-tertiary bg-opacity-10 dark:bg-opacity-10 backdrop-blur-sm z-[999]"
-          aria-hidden="true"
-        />
+        <div className={styles.backdrop} aria-hidden="true" />
       </Transition.Child>
 
       <Transition.Child
@@ -44,18 +42,14 @@ const FeaturedLists = ({
         leaveFrom="transform scale-100 opacity-100"
         leaveTo="transform scale-95 opacity-0"
       >
-        <Dialog
-          open={open}
-          onClose={toggleOpen}
-          className="fixed inset-0 flex items-center justify-center z-[999]"
-        >
-          <Dialog.Panel className="border border-light-bg-secondary dark:border-dark-bg-tertiary w-3/4 sm:w-1/2 rounded-lg shadow-md bg-light-bg-primary dark:bg-dark-bg-secondary">
-            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-              <h3 className="dark:text-dark-text-primary">{title}</h3>
+        <Dialog open={open} onClose={toggleOpen} className={styles.dialog}>
+          <Dialog.Panel className={styles.panel}>
+            <div className={styles.header}>
+              <h3 className={styles.title}>{title}</h3>
               <button
                 onClick={toggleOpen}
                 type="button"
-                className="hover:bg-light-bg-secondary dark:text-dark-text-primary rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center dark:hover:bg-dark-bg-tertiary"
+                className={styles.close}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -65,7 +59,7 @@ const FeaturedLists = ({
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  className="w-5 h-5"
+                  className={styles.closeIcon}
                 >
                   <line x1="18" y1="6" x2="6" y2="18"></line>
                   <line x1="6" y1="6" x2="18" y2="18"></line>
@@ -74,10 +68,8 @@ const FeaturedLists = ({
                 <span className="sr-only">Close modal</span>
               </button>
             </div>
-            <div className="p-4">
-              <button className="text-once hover:text-light-bg-primary hover:bg-once inline-flex w-full justify-center font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-dark-bg-primary hover:dark:text-dark-bg-primary dark:bg-once-dark hover:dark:bg-once-hover">
-                Try it now
-              </button>
+            <div className={styles.buttonContainer}>
+              <button className={styles.try}>Try it now</button>
             </div>
           </Dialog.Panel>
         </Dialog>
