71dbbc0c650c8a02017aa9f9dbb73cf17f61d2cd
sonastea <sonastea@gmail.com>
2025-02-16 23:20:48 -0800

    fix: featured lists randomness algorithm

--- src/backend/prisma/client.ts
+++ src/backend/prisma/client.ts
@@ -1,31 +1,21 @@
 import { Prisma, PrismaClient } from "@prisma/client";
+import prismaRandom from "prisma-extension-random";
 
-declare global {
-  var prisma:
-    | PrismaClient<
-        Prisma.PrismaClientOptions,
-        "query" | "info" | "warn" | "error"
-      >
-    | undefined;
-}
+const basePrisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+  ],
+});
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: [
-      {
-        emit: "event",
-        level: "query",
-      },
-      {
-        emit: "stdout",
-        level: "error",
-      },
-    ],
-  });
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
-
-prisma.$on("query", (e: Prisma.QueryEvent) => {
+basePrisma.$on("query", (e: Prisma.QueryEvent) => {
   console.log("Query: " + e.query + " [Duration: " + e.duration + "ms]");
 });
+
+export const prisma = basePrisma.$extends(prismaRandom());
--- src/backend/router/listing.ts
+++ src/backend/router/listing.ts
@@ -11,6 +11,9 @@ const nanoid = customAlphabet(
   16
 );
 
+const TOP_K_LISTS = 3;
+const DAYS_AGO = 5;
+
 const RedisExpireTime: number = 7 * (60 * 60 * 24); // expire time in days from seconds
 
 const redis = new Redis(process.env.REDIS_URL as string, {
@@ -18,6 +21,19 @@ const redis = new Redis(process.env.REDIS_URL as string, {
 })
   .on("error", (err) => console.error("Redis error: ", err.message))
   .on("connect", () => console.log("Redis is connected."));
+
+const FeaturedLists = Prisma.validator<Prisma.ListingDefaultArgs>()({
+  select: {
+    label: true,
+    title: true,
+    items: {
+      select: {
+        id: true,
+        value: true,
+      },
+    },
+  },
+});
 
 const ListType = Prisma.validator<Prisma.ListingDefaultArgs>()({
   select: {
@@ -99,64 +115,49 @@ export const listingRouter = router({
       return list;
     }),
   getFeatured: publicProcedure.query(async () => {
-    const topKLists = 3;
-    let featured: any[] = [];
-    let daysAgo = 5;
-    let kDaysAgo;
+    const kDaysAgo = new Date();
+    kDaysAgo.setDate(kDaysAgo.getDate() - DAYS_AGO);
 
-    kDaysAgo = new Date(new Date().setDate(new Date().getDate() - daysAgo));
-
-    featured = await prisma.listing.findMany({
-      select: {
-        label: true,
-        title: true,
-        items: {
-          select: {
-            id: true,
-            value: true,
+    const getFeatured = async (kDaysAgo: Date) => {
+      const featured = await prisma.listing.findMany({
+        ...FeaturedLists,
+        orderBy: {
+          visits: {
+            _count: "desc",
           },
         },
-      },
-      orderBy: {
-        visits: {
-          _count: "desc",
-        },
-      },
-      where: {
-        createdAt: {
-          gte: kDaysAgo,
-        },
-        visits: {
-          some: {
-            createdAt: {
-              gte: kDaysAgo,
+        where: {
+          createdAt: {
+            gte: kDaysAgo,
+          },
+          visits: {
+            some: {
+              createdAt: {
+                gte: kDaysAgo,
+              },
             },
           },
         },
-      },
-      take: topKLists,
-    });
-
-    if (featured.length < topKLists) {
-      const totalLists = await prisma.listing.count();
-      const skip = Math.floor(Math.random() * totalLists);
-      featured = await prisma.listing.findMany({
-        select: {
-          label: true,
-          title: true,
-          items: {
-            select: {
-              id: true,
-              value: true,
-            },
-          },
-        },
-        skip,
-        take: 3,
+        take: TOP_K_LISTS,
       });
-    }
 
-    return featured;
+      if (featured?.length < TOP_K_LISTS) {
+        const needed = TOP_K_LISTS - featured.length;
+        const excluded = featured.map((list) => list.label);
+        const randomListings = await prisma.listing.findManyRandom(needed, {
+          ...FeaturedLists,
+          where: {
+            label: {
+              notIn: excluded,
+            },
+          },
+        });
+
+        return [...featured, ...randomListings];
+      }
+    };
+
+    return await getFeatured(kDaysAgo);
   }),
   create: publicProcedure
     .input(
