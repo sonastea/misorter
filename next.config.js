371b21e3d35d66e1943a1ed80bdf20ff76c6c32d
sonastea <sonastea@gmail.com>
2025-03-19 17:57:05 -0700

    chore!: update dependencies & remove @ducanh2912/next-pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require("@ducanh2912/next-pwa").default({
-  disable: process.env.NODE_ENV === "development",
-  dest: "public",
-});
-
-const { withAxiom } = require("next-axiom");
+import { withAxiom } from "next-axiom";
 
 module.exports = withAxiom(
   withPWA({
